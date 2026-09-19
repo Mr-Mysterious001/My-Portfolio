70db16b399bc4d3f649a751f2c6356274f0b2a77
MR-Mysterious001 <chandrakshay13@gmail.com>
2026-09-20 00:19:38 +0530

    added profile pic

--- src/App.jsx
+++ src/App.jsx
@@ -408,7 +408,7 @@ function App() {
                 <div className="orbit orbit-b" />
                 <div className="orbit orbit-c" />
                 <div className="avatar-ring">
-                  <img src="/self.png" alt="Akshay Chandra" />
+                  <img src="/assets/self.png" alt="Akshay Chandra" />
                 </div>
               </div>
 
